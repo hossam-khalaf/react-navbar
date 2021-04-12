c40917aeb58c97eca0d2f140b4fd6c4dd1642e85
hossam-khalaf <hossamhassan303089@gmail.com>
2021-04-12 16:26:56 +0200

    clean up && Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import '../App.css'
+
+const Navbar = () => {
+	return (
+		<div className='navbar'>
+			<div className='left'>left</div>
+			<div className='right'>Rt</div>
+		</div>
+	)
+}
+
+export default Navbar
